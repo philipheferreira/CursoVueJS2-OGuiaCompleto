85c8d1ed70e923b4f627e17cd659f8ded3e09a32
philipheferreira <philipheferreira@gmail.com>
2025-02-13 14:17:59 -0300

    Feat: created basic template from module 7 cap 6

---
+++ 7-Introdução aos componentes/6-usando-componentes/src/Contador.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'ContadorVue',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 7-Introdução aos componentes/6-usando-componentes/src/Contadores.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'ContadoresVue',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
