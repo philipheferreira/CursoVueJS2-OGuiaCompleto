d8b4176566831481c2d3c67720d8e3cb7c429886
philipheferreira <philipheferreira@gmail.com>
2025-02-13 13:11:52 -0300

    Feat: module 7 cap 4 finish

--- 7-Introdução aos componentes/4 -Criar-Projeto-Usando-Vue-CLI/src/App.vue
+++ 7-Introdução aos componentes/4 -Criar-Projeto-Usando-Vue-CLI/src/App.vue
@@ -1,10 +1,25 @@
+<!-- Criação da primeira funcionalidade pelo app-->
+
 <template>
-  
+    <div id="app">
+      <h2>Contadores</h2>
+
+      {{ contador }}
+    </div>
 </template>
 
 <script>
 export default {
 
+  // data: { // forma errada, não ira funcionar de jeito nenhum
+  //  contador: 0
+  // }
+
+  data() {
+    return { 
+      contador: 0
+    }
+  }
 }
 </script>
 
