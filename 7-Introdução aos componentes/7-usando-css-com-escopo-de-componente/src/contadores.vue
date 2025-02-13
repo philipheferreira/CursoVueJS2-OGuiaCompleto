435c834174347327e4ff9afc39a1e27d0863b7bf
philipheferreira <philipheferreira@gmail.com>
2025-02-13 17:39:18 -0300

    Feat: module 7 cap 7

---
+++ 7-Introdução aos componentes/7-usando-css-com-escopo-de-componente/src/contadores.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contadores">
+        <app-contador></app-contador>
+    </div>
+</template>
+
+<script>
+import Contador from './contador.vue'
+export default {
+    name: 'ContadoresVue',
+    components: {
+        'app-contador': Contador // component que está sendo puxado de contador
+    }
+}
+</script>
+
+<style>
+
+</style>
