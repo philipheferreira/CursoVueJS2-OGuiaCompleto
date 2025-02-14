d89fd3529bd73d86cd169ad9a891ba9b3ecf5f2b
philipheferreira <philipheferreira@gmail.com>
2025-02-14 19:14:26 -0300

    Feat: module 7 cap 11 create basic template

--- 7-Introdução aos componentes/11-regras-de-nomes-de-componentes/src/App.vue
+++ 7-Introdução aos componentes/11-regras-de-nomes-de-componentes/src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-    
+
+    <app-footer></app-footer>
+    <app-menu></app-menu>
+    <app-toolbar></app-toolbar>
+    <app-dropdown></app-dropdown>
+    <app-carousel></app-carousel>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Footer from '../src/components/template/Footer.vue'
+import Menu from '../src/components/template/Menu.vue'
+import Toolbar from '../src/components/template/Toolbar.vue'
+import Dropdown from '../src/components/user/Dropdown.vue'
+import Carousel  from './components/widgets/Carousel.vue' 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'app-footer' : Footer,
+    'app-menu' : Menu,
+    'app-toolbar' : Toolbar,
+    'app-dropdown' : Dropdown,
+    'app-carousel' : Carousel
   }
 }
 </script>
--- 7-Introdução aos componentes/11-regras-de-nomes-de-componentes/src/components/widgets/Carousel.vue
+++ 7-Introdução aos componentes/11-regras-de-nomes-de-componentes/src/components/widgets/Carousel.vue
@@ -1,12 +1,10 @@
 <template>
-    <div>
-        
-    </div>
+    <div>Ola, eu sou um carrosel</div>
 </template>
 
 <script>
 export default {
-
+    name: 'carouselVue'
 }
 </script>
 
