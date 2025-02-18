bfc46454722187126c54a04862bcdfb6b0d373fb
philipheferreira <philipheferreira@gmail.com>
2025-02-18 11:24:52 -0300

    Feat: module 8 cap 6 finish

--- 8-comunicação entre componentes/6-usando-props-no-componente-filho/src/components/UsuarioInfo.vue
+++ 8-comunicação entre componentes/6-usando-props-no-componente-filho/src/components/UsuarioInfo.vue
@@ -5,7 +5,7 @@
         <h2>As informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do usuario pelo props e estatico: <strong> {{ nome }} </strong> </p> <!-- Forma mais direta e pratica de passar pela props -->
-        <p>Nome do usuario sendo chamado por binding criado na variavel principal  : <strong> {{ nome1 }} </strong>  </p>
+        <p>Nome do usuario sendo chamado por binding criado na variavel principal  : <strong> {{ inverterNome(nome1) }} </strong>  </p> <!-- Existe a partir das propriedades existe a possibilidade de trabalhar dados dentro de data -->
     </div>
 
 </template>
@@ -15,7 +15,12 @@
 
     export default{
         name: 'usuarioInfo',
-        props: ['nome', 'nome1']
+        props: ['nome', 'nome1'],
+        methods: {
+            inverterNome(palavra){
+                return palavra.split('').reverse().join() //inverter o nome que foi passado, que está sendo usado palavra dentro de metodos
+            }
+        }
     }
 
 </script>
