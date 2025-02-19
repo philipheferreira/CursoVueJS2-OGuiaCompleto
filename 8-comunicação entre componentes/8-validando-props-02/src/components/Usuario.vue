a317196fb8a9356c5b32919b33e9f59508eee038
philipheferreira <philipheferreira@gmail.com>
2025-02-19 09:53:22 -0300

    Feat: module 8 cap 8 create basic template for start

---
+++ 8-comunicação entre componentes/8-validando-props-02/src/components/Usuario.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <div class="componentes">
+
+            <app-usuario-info :nome="nomeCompleto" v-bind:nome1="nomeCompleto" /> <!-- Dentro de aspas ele vai interpretar como um string, se eu mandar true ou false será interpretado como boolean. Mandando numero ira como number -->
+            <app-usuario-info nome="1" v-bind:nome1="nomeCompleto" />
+            <app-usuario-editar />
+
+        </div>
+
+    </div>
+
+
+</template>
+
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo.vue'
+import AppUsuarioEditar from './UsuarioEditar.vue'
+
+export default {
+    name: 'appUsuario',
+    components: {
+        AppUsuarioInfo,
+        AppUsuarioEditar
+    },
+    data(){
+        return {
+            nomeCompleto: 'Philiphe Siqueira Ferreira'
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+.container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+
+</style>
---
+++ 8-comunicação entre componentes/8-validando-props-02/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div class="componente">
+
+        <h2>As informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuario pelo props e estatico: <strong> {{ nome }} </strong> </p> <!-- Forma mais direta e pratica de passar pela props -->
+        <p>Nome do usuario sendo chamado por binding criado na variavel principal  : <strong> {{ inverterNome(nome1) }} </strong>  </p> <!-- Existe a partir das propriedades existe a possibilidade de trabalhar dados dentro de data -->
+    </div>
+
+</template>
+
+
+<script>
+
+    export default{
+        name: 'usuarioInfo',
+        // props: ['nome', 'nome1'], //Forma mais geral de receber as coisas
+        props: {
+            // nome: String // Define que espera receber uma String com os dados
+            nome: { // Cria um objeto de informações e definições dentro da variavel para receber melhor os valores props com validações
+                type: String, // Informa que a variavel deve receber uma string 
+                // required: true, // Diz que a variavel é obrigatoria, ou seja, ela deve ser repassada
+                default: 'Anonimo' // Caso
+            }
+        },
+        methods: {
+            inverterNome(palavra){
+                return palavra.split('').reverse().join() //inverter o nome que foi passado, que está sendo usado palavra dentro de metodos
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+
+</style>
