0988559b584beb76ea363b944fd478120398cf3c
philipheferreira <philipheferreira@gmail.com>
2025-02-19 11:31:55 -0300

    Feat: correction module 8 cap 9

--- 8-comunicação entre componentes/9-comunicacao-indireta-com-eventos-personalizados/src/components/UsuarioInfo.vue
+++ 8-comunicação entre componentes/9-comunicacao-indireta-com-eventos-personalizados/src/components/UsuarioInfo.vue
@@ -5,7 +5,7 @@
         <h2>As informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do usuario pelo props e estatico: <strong> {{ nome }} </strong> </p> <!-- Forma mais direta e pratica de passar pela props -->
-        <p>Nome do usuario sendo chamado por binding criado na variavel principal  : <strong> {{ inverterNome(nome1) }} </strong>  </p> <!-- Existe a partir das propriedades existe a possibilidade de trabalhar dados dentro de data -->
+        <p>Nome do usuario sendo chamado por binding criado na variavel principal  : <strong> {{ nome1 }} </strong>  </p> <!-- Existe a partir das propriedades existe a possibilidade de trabalhar dados dentro de data -->
     </div>
 
 </template>
@@ -20,11 +20,11 @@
             // nome: String // Define que espera receber uma String com os dados
             nome: { // Cria um objeto de informações e definições dentro da variavel para receber melhor os valores props com validações
                 type: String, // Informa que a variavel deve receber uma string 
-                // required: true, // Diz que a variavel é obrigatoria, ou seja, ela deve ser repassada
+                 required: true, // Diz que a variavel é obrigatoria, ou seja, ela deve ser repassada
                 // default: 'Anonimo' // Caso
-                default: function() { // Cria o default com base em uma função que eu queria da minha maneira
+                /* default: function() { // Cria o default com base em uma função que eu queria da minha maneira
                     return Array(10).fill(0).join(',') //Usei uma logica maluca pra gerar um valor 0, 10 vezes separado por virgulas
-                }
+                 } */
             }
         },
         methods: {
