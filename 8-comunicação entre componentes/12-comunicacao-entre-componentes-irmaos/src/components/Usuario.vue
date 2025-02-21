7ace6474860458ed0dacfa25ed12979c054d1f6f
philipheferreira <philipheferreira@gmail.com>
2025-02-21 13:58:23 -0300

    Feat: module 8 cap 11 finish basic template

---
+++ 8-comunicação entre componentes/12-comunicacao-entre-componentes-irmaos/src/components/Usuario.vue
@@ -0,0 +1,76 @@
+ <!-- Comando necessario para fazer a comunicação com o componente de forma inversa. Usando callback para comunicar filho com o pai -->
+
+ <template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong> {{ nomeCompleto }} </strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <div class="componentes">
+
+            <app-usuario-info 
+            :nome="nomeCompleto" 
+            v-bind:nome1="nomeCompleto" 
+            @reiniciar-nome="reiniciarNome"
+            :reiniciarFn="reiniciarNomePrincipal"
+            /> <!-- Dentro de aspas ele vai interpretar como um string, se eu mandar true ou false será interpretado como boolean. Mandando numero ira como number -->
+            <app-usuario-info nome="1" v-bind:nome1="nomeCompleto" @reiniciar-nome="reiniciarNome"/>
+            <app-usuario-editar />
+
+        </div>
+
+    </div>
+
+
+</template>
+
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo.vue'
+import AppUsuarioEditar from './UsuarioEditar.vue'
+
+export default {
+    name: 'appUsuario',
+    components: {
+        AppUsuarioInfo,
+        AppUsuarioEditar
+    },
+    data(){
+        return {
+            nomeCompleto: 'Philiphe Siqueira Ferreira'
+        }
+    },
+    methods: {
+        alterarNome(){
+            this.nomeCompleto = 'Ana'
+        },
+        reiniciarNomePrincipal() { // Função criada para se comunicar com o componente filho
+            this.nomeCompleto = 'Philiphe Siqueira Ferreira'
+        },
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+.container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+
+</style>
