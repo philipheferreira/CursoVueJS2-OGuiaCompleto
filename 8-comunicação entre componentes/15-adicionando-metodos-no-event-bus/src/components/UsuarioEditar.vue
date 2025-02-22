5c1cb1a327e898a6f9c61b5431f68acba9e9a2de
philipheferreira <philipheferreira@gmail.com>
2025-02-22 16:53:07 -0300

    Feat: module 8 cap 15

---
+++ 8-comunicação entre componentes/15-adicionando-metodos-no-event-bus/src/components/UsuarioEditar.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="componente">
+
+        <h2>Alterar os dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p> Idade do usuario: <strong> {{ idade }} </strong>  </p>
+        <button @click="alterarIdade"> Alterar Idade</button>
+    </div>
+
+</template>
+
+<script>
+import barramento from '@/barramento'
+    export default{
+        name: 'usuarioEditar',
+        props: { 
+            idade: Number
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            alterarIdade(){
+           /* eslint-disable */ this.idade += 1
+            barramento.$emit('idadeMudou', 33) // Emite o evento para o barramento
+            }
+        }
+
+    }
+
+</script>
+
+
+<style scoped>
+
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+
+</style>
