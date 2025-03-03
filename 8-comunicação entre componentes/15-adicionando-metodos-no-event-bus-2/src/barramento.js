14082f6d7e311da67d55467f7a7aa77aee101b71
philipheferreira <philipheferreira@gmail.com>
2025-03-03 11:22:46 -0300

    Feat: things i forget

--- 8-comunicação entre componentes/15-adicionando-metodos-no-event-bus-2/src/barramento.js
+++ 8-comunicação entre componentes/15-adicionando-metodos-no-event-bus-2/src/barramento.js
@@ -1,2 +1,11 @@
 import Vue from 'vue'
-export default new Vue()
+export default new Vue({
+    methods: {
+        alterarIdade(idade) {
+            this.$emit('idadeMudou', idade) // Essa função emite a operação
+        },
+        quandoIdadeMudar(callback) {
+            this.$on('idadeMudou', callback) // é um evento de callback. É chamada toda vez que o evento acontecer
+        }
+    } 
+})
