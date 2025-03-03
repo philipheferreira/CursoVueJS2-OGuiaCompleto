e79b8786715fc416d141a8e9d220b3ebb10277a1
philipheferreira <philipheferreira@gmail.com>
2025-03-03 09:58:46 -0300

    Feat: module 8 cap 15 commits for identification

--- 8-comunicação entre componentes/15-adicionando-metodos-no-event-bus/src/barramento.js
+++ 8-comunicação entre componentes/15-adicionando-metodos-no-event-bus/src/barramento.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 export default new Vue({
     methods: {
         alterarIdade(idade) {
-            this.$emit('idadeMudou', idade)
+            this.$emit('idadeMudou', idade) // Essa função emite a operação
         },
         quandoIdadeMudar(callback) {
-            this.$on('idadeMudou', callback)
+            this.$on('idadeMudou', callback) // é um evento de callback. É chamada toda vez que o evento acontecer
         }
     } 
 })
