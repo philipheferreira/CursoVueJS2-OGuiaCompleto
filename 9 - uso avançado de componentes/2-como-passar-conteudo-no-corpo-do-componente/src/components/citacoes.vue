df9ef44f919324d88be7258889b8c77e9dba69ce
philipheferreira <philipheferreira@gmail.com>
2025-03-06 12:06:43 -0300

    Feat: module 9 cap 2 create basic template for start

---
+++ 9 - uso avançado de componentes/2-como-passar-conteudo-no-corpo-do-componente/src/components/citacoes.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        {{ numero }} {{ indice }} <!-- Numero é uma variavel criada dentro do component e indice é uma operação gerada dentro do computed -->
+        {{ citacoes[indice].texto }} <!-- Faz com que eu tenha acesso a lista citações, usando o metodo indice dentro do computed fazendo o controle das operações -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "citacoesComponent",
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
