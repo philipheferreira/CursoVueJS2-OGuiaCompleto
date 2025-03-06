b02eac2ca5e878e169d4f3e3877219fd8fe6fd66
philipheferreira <philipheferreira@gmail.com>
2025-03-06 13:59:52 -0300

    Feat: module 9 cap 3 create basic template for start

---
+++ 9 - uso avançado de componentes/3-passando-conteudo-com-slots/src/components/citacao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="citacao">
+        {{ texto }}
+    </div>
+</template>
+
+<script>
+export default {
+    name:"citacaoComponent",
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
---
+++ 9 - uso avançado de componentes/3-passando-conteudo-com-slots/src/components/citacoes.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <Citacao :texto="citacoes[indice].textoLocal"></Citacao> <!-- Enviando os dados para o props do componente citacao receber -->
+    </div>
+</template>
+
+<script>
+import Citacao from './citacao.vue' // importa componente citacao para o codigo
+
+export default {
+    name: "citacoesComponent",
+    components: {
+         Citacao 
+        },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                textoLocal: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                textoLocal: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                textoLocal: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3) // Funcao indice retorna o numero. Toda vez que ele for igual a 3, zero o seu valor
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
