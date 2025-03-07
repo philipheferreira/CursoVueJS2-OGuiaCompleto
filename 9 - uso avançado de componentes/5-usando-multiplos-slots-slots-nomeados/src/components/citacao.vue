c8f5fda71157dec7c418313b9a599424470c6ae0
philipheferreira <philipheferreira@gmail.com>
2025-03-07 14:37:17 -0300

    Feat: module 9 cap 5 prepare for start project

---
+++ 9 - uso avançado de componentes/5-usando-multiplos-slots-slots-nomeados/src/components/citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"citacaoComponent",
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    h1 {
+        color: red; /* Crio uma style dentro desse compontonete que será mostrado no outro componente citacoes, que está chamando ele */
+        /* color: red!important; Caso queira, posso utilizar o comando important que ira sobreescrever o conflito de h1 do outro componente, mas não é uma pratica recomendada */ 
+    }
+</style>
---
+++ 9 - uso avançado de componentes/5-usando-multiplos-slots-slots-nomeados/src/components/citacoes.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <Citacao>
+            <h1> {{ citacoes[indice].autor }} </h1>
+            <p> {{ citacoes[indice].textoLocal }} </p>
+            <h6> {{ citacoes[indice].autor }} </h6>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './citacao.vue'
+
+export default {
+    name: "citacoesComponent",
+    components: {
+         Citacao 
+        },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                textoLocal: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                textoLocal: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                textoLocal: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3) // Funcao indice retorna o numero. Toda vez que ele for igual a 3, zero o seu valor
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        color: green; /* Por mais que o componente chamado citacoes tem um h1 style proprio, ele será sobreescrito por esse*/
+    }
+</style>
