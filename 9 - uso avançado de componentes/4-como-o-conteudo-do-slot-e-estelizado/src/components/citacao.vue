20c70ced3eac68ddf8a59cc95e270a0524d03df6
philipheferreira <philipheferreira@gmail.com>
2025-03-07 11:14:02 -0300

    Feat: module 9 cap 4 create basic start tempalte

---
+++ 9 - uso avançado de componentes/4-como-o-conteudo-do-slot-e-estelizado/src/components/citacao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="citacao">
+        <slot></slot> <!-- Comando exclusivo do Vue para exibir aquilo que foi passado dentro do corpo do componente em outros componentes que chamem ele. Slot recebe tudo que foi declarado em outro componente para ele receber -->
+    </div>
+</template>
+
+<script>
+export default {
+    name:"citacaoComponent",
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
