61b3f257c927a176cbc85d3c10b2dc29da3d6734
philipheferreira <philipheferreira@gmail.com>
2025-03-08 10:53:35 -0300

    Feat: module 9 cap 8 create basic template

---
+++ 9 - uso avançado de componentes/8-alternando-entre-multiplos-componentes-com-componentes-dinamicos/src/components/citacao.vue
@@ -0,0 +1,28 @@
+<!-- Reiterando que slot é muito rico e pratico para as funcionalidades -->
+
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot> 
+        <div class="autor"><slot name="autor"></slot></div> <!-- Criando uma class autor para modificar o slot nomeado autor -->
+    </div>
+</template>
+
+<script>
+export default {
+    name:"citacaoComponent",
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    .autor{ /* a class autor é afetada pelas modificações aqui geradas */
+        float: right;
+    }
+</style>
