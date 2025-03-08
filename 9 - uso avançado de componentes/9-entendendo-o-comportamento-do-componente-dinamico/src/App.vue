804c9b3ee0374e8cddd81b93dae198d6dd2f40d4
philipheferreira <philipheferreira@gmail.com>
2025-03-08 17:34:11 -0300

    Feat: module 9 cap 9 create components

--- 9 - uso avançado de componentes/9-entendendo-o-comportamento-do-componente-dinamico/src/App.vue
+++ 9 - uso avançado de componentes/9-entendendo-o-comportamento-do-componente-dinamico/src/App.vue
@@ -1,24 +1,45 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<span>
+			<button @click="componente = 'citacoes'">Citações</button> <!-- O botão passa para o componente o valor passe, que repassa para o componente component alterando seu valor -->
+			<button @click="componente = 'sobre'">Sobre</button> <!-- O botão passa para o componente o valor passe, que repassa para o componente component alterando seu valor -->
+		</span>
+		<component :is="componente"/> <!-- Componente especial do Vue para receber um componente especifico, no is será definido o componente que ira receber o component -->
+	</div>
 </template>
 
 <script>
-
+import Citacoes from './components/citacoes'
+import Sobre from './components/sobre.vue'
 
 export default {
-  name: 'App',
-  components: {}
+	components: { Citacoes, Sobre },
+	data() {
+		return {
+			componente: 'citacoes' // variavel que recebe o nome do componeentee
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 1.6rem;
+	}
+
+	button {
+        outline: none;
+        padding: 5px 15px 10px;
+        margin: 10px 5px;
+        border-radius: 3px;
+        font-size: 2rem;
+        font-weight: 300;
+        color: #FFF;
+        background-color: rgba(0, 0, 0, .5);
+    }
 </style>
