91e73a4a1474cd5772cdcfc42802a4a572eb1f13
philipheferreira <philipheferreira@gmail.com>
2025-03-08 17:31:16 -0300

    Feat: module 9 cap 9 finish

--- 9 - uso avançado de componentes/8-alternando-entre-multiplos-componentes-com-componentes-dinamicos/src/App.vue
+++ 9 - uso avançado de componentes/8-alternando-entre-multiplos-componentes-com-componentes-dinamicos/src/App.vue
@@ -1,7 +1,10 @@
 <template>
 	<div id="app">
-		<citacoes />
-		<sobre />
+		<span>
+			<button @click="componente = 'citacoes'">Citações</button> <!-- O botão passa para o componente o valor passe, que repassa para o componente component alterando seu valor -->
+			<button @click="componente = 'sobre'">Sobre</button> <!-- O botão passa para o componente o valor passe, que repassa para o componente component alterando seu valor -->
+		</span>
+		<component :is="componente"/> <!-- Componente especial do Vue para receber um componente especifico, no is será definido o componente que ira receber o component -->
 	</div>
 </template>
 
@@ -10,7 +13,12 @@ import Citacoes from './components/citacoes'
 import Sobre from './components/sobre.vue'
 
 export default {
-	components: { Citacoes, Sobre }
+	components: { Citacoes, Sobre },
+	data() {
+		return {
+			componente: 'citacoes' // variavel que recebe o nome do componeentee
+		}
+	}
 }
 </script>
 
