d60f368d466df6f38168202a4f5b8a035f8f7aa8
philipheferreira <philipheferreira@gmail.com>
2025-03-10 11:57:52 -0300

    Feat: module 9 cap 11 basic template for start

---
+++ 9 - uso avançado de componentes/11-metodos-de-ciclo-de-vida-de-um-componente-dinamico/src/components/citacao.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot> 
+        <div class="autor"><slot name="autor"></slot></div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"citacaoComponent",
+    created(){
+        console.log('Component Citação criada')
+    },
+    destroyed(){
+        console.log("Component Citação destroida")
+    },
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    .autor{
+        float: right;
+    }
+</style>
