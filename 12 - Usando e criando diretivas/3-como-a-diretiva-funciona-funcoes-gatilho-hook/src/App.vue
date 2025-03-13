f6b305a7d248cf23d393139b23878282eb4ad595
philipheferreira <philipheferreira@gmail.com>
2025-03-13 10:08:07 -0300

    Feat: module 12 cap 3 basic template for start

--- 12 - Usando e criando diretivas/3-como-a-diretiva-funciona-funcoes-gatilho-hook/src/App.vue
+++ 12 - Usando e criando diretivas/3-como-a-diretiva-funciona-funcoes-gatilho-hook/src/App.vue
@@ -1,6 +1,12 @@
+<!-- Essa aula tem o foco de estudar o funcionamento dos Hooks -->
+
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<h1>Diretivas</h1>
+    <hr/>
+    <p v-text="'Usando diretiva para v-text'"></p>
+    <p v-html="'Usando diretiva <strong> V-html </strong>'"></p>
+	</div>
 </template>
 
 <script>
