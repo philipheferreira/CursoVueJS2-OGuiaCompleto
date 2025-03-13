d7d6f2f205115df6f3e954dab0044067fa7a7fa0
philipheferreira <philipheferreira@gmail.com>
2025-03-13 10:04:29 -0300

    Feat: module 12 cap 2 finish

--- 12 - Usando e criando diretivas/2-entendendo-diretivas/src/App.vue
+++ 12 - Usando e criando diretivas/2-entendendo-diretivas/src/App.vue
@@ -1,6 +1,11 @@
+<!-- O foco dessa aula é sobre diretivas -->
+
 <template>
 	<div id="app">
 		<h1>Diretivas</h1>
+    <hr/>
+    <p v-text="'Usando diretiva para v-text'"></p> <!-- Diretiva que print um objeto na pagina. Ele está esperando uma string então será impresso na tela -->
+    <p v-html="'Usando diretiva <strong> V-html </strong>'"></p> <!-- É uma diretiva que printa um texto na tela e dentro pode usar comandos de html -->
 	</div>
 </template>
 
