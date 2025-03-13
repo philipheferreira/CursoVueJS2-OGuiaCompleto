75d2436e222aa5ff785e0e255ce60ebbb11c2f8c
philipheferreira <philipheferreira@gmail.com>
2025-03-13 15:03:16 -0300

    Feat: module 12 cap 7 create basic start template

--- 12 - Usando e criando diretivas/7-modificando-diretivas-personalizadas-com-modificadores/src/App.vue
+++ 12 - Usando e criando diretivas/7-modificando-diretivas-personalizadas-com-modificadores/src/App.vue
@@ -1,17 +1,30 @@
+<!-- Exemplo de organização de uma diretiva, no caso sua anatomia:
+  v-teste:argumento.mod1.mod2.mod3="'valor'"
+
+-->
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>Diretivas</h1>
+    <hr/>
+    <p v-text="'Usando diretiva para v-text'"></p>
+    <p v-html="'Usando diretiva <strong> V-html </strong>'"></p>
+    <hr>
+    <p v-destaque="'red'">Usando diretiva personalizada</p>
+    <p v-destaque="cor">Usando diretiva personalizada</p>
+    <p v-destaque:fundo="cor">Usando diretiva personalizada</p> <!-- Usando um argumento dentro da diretiva para selecionar a opção mais viavel na chamada -->
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      cor: 'blue',
+      cor2: 'green'
+    }
   }
 }
 </script>
--- 12 - Usando e criando diretivas/7-modificando-diretivas-personalizadas-com-modificadores/src/main.js
+++ 12 - Usando e criando diretivas/7-modificando-diretivas-personalizadas-com-modificadores/src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('destaque', {
+  bind(el, binding){
+    //el.style.backgroundColor = 'lightgreen'
+
+    if(binding.arg === 'fundo'){ // O if verifica o argumento no binding, se for igual a fundo então essa opção sera ativada
+      el.style.backgroundColor = binding.value
+    }else{ // qualquer outra situação será essa outra opção que é a cor do texto
+      el.style.color = binding.value
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
