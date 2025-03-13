89365a61e37a98f970472481d3db5bf71b5f1085
philipheferreira <philipheferreira@gmail.com>
2025-03-13 13:08:31 -0300

    Feat: module 12 cap 6 basic template for start

--- 12 - Usando e criando diretivas/6-passando-argumento-para-diretiva-personalizada/src/main.js
+++ 12 - Usando e criando diretivas/6-passando-argumento-para-diretiva-personalizada/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('destaque', { // criação de uma diretiva usando hook
+  bind(el, binding){
+    //el.style.backgroundColor = 'lightgreen' // a partir da diretiva consigo modificar o css
+    el.style.backgroundColor = binding.value // Usando a variavel binding é possivel recever variaveus na chamada das diretivas quando forem criadas
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
