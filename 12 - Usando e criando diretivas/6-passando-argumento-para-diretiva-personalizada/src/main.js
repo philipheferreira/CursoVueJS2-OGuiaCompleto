1fba87750f49c4dd90c5c090732e873b8f3a6d58
philipheferreira <philipheferreira@gmail.com>
2025-03-13 14:04:47 -0300

    Feat: module 12 cap 6 delete useless comments

--- 12 - Usando e criando diretivas/6-passando-argumento-para-diretiva-personalizada/src/main.js
+++ 12 - Usando e criando diretivas/6-passando-argumento-para-diretiva-personalizada/src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.directive('destaque', { // criação de uma diretiva usando hook
+Vue.directive('destaque', {
   bind(el, binding){
-    //el.style.backgroundColor = 'lightgreen' // a partir da diretiva consigo modificar o css
-    el.style.backgroundColor = binding.value // Usando a variavel binding é possivel recever variaveus na chamada das diretivas quando forem criadas
+    //el.style.backgroundColor = 'lightgreen'
+    el.style.backgroundColor = binding.value
   }
 })
 
