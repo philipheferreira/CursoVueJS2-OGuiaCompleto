405b4e17fff5f2f8af3eba5d6e852d513f6e8055
philipheferreira <philipheferreira@gmail.com>
2025-03-14 15:03:31 -0300

    Feat: module 13 cap 3 create basic template for start

--- 13 - Melhorando sua App com filtros e mixins/3-filtro-gloval-e-como-encadear-multiplos-filtros/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/3-filtro-gloval-e-como-encadear-multiplos-filtros/src/App.vue
@@ -1,28 +1,38 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+    <hr>
+    <p> {{ cpf | cpf }} </p> 
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  filters: {
+    cpf(valor){ 
+      const arr = valor.split('')
+      arr.splice(3, 0, '.')
+      arr.splice(7, 0, '.') 
+      arr.splice(11, 0, '-')
+      return arr.join('') 
+    }
+  },
+  data() {
+    return {
+      cpf: '60070080090'
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
 }
 </style>
