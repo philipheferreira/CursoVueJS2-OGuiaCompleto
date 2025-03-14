149ccb05a318503d6ad09c4425e2bcc77a727164
philipheferreira <philipheferreira@gmail.com>
2025-03-14 14:27:19 -0300

    Feat: module 13 cap 2 create basic template

--- 13 - Melhorando sua App com filtros e mixins/2-criando-um-filtro-local/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/2-criando-um-filtro-local/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {}
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
 }
 </style>
