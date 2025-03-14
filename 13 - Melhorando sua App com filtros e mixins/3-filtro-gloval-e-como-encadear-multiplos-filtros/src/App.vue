1c150c7a362c8b301509d2a0eeabedb10a83f452
philipheferreira <philipheferreira@gmail.com>
2025-03-14 15:30:32 -0300

    Feat: module 13 cap 3 finish

--- 13 - Melhorando sua App com filtros e mixins/3-filtro-gloval-e-como-encadear-multiplos-filtros/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/3-filtro-gloval-e-como-encadear-multiplos-filtros/src/App.vue
@@ -2,15 +2,18 @@
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
     <hr>
-    <p> {{ cpf | cpf }} </p> 
+    <p> CPF: {{ cpfAluno }} </p>
+    <p> CPF após o filtro cpf: {{ cpfAluno || acaoCPF }} </p>
+    <p> CPF após o filtro cpf com a inversão: {{ cpfAluno | acaoCPF | inverter}} </p> <!-- O inverte é a chamada de mais um filtro em conjunto. Fazendo a separação por cpf e invertendo o resultado depois -->
+    <p> CPF apenas com o filtro da inversão:  {{ cpfAluno | inverter}} </p> 
 	</div>
 </template>
 
 <script>
 export default {
   filters: {
-    cpf(valor){ 
-      const arr = valor.split('')
+    acaoCPF(valor){ 
+      const arr = valor.split('') // `${valor}` => Utilizando esse comando backstring para assim mesmo que receba um valor inteiro ele faz a conversão automaticamente
       arr.splice(3, 0, '.')
       arr.splice(7, 0, '.') 
       arr.splice(11, 0, '-')
@@ -19,7 +22,7 @@ export default {
   },
   data() {
     return {
-      cpf: '60070080090'
+      cpfAluno: '60070080090'
     }
   }
 }
