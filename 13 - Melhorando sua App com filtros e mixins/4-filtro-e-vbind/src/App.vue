e266d1d1adb4deba40fcdf738e8e8d3dadf46d32
philipheferreira <philipheferreira@gmail.com>
2025-03-14 15:33:08 -0300

    Feat: module 13 cap 4 create basic template for start

--- 13 - Melhorando sua App com filtros e mixins/4-filtro-e-vbind/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/4-filtro-e-vbind/src/App.vue
@@ -1,23 +1,41 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+    <hr>
+    <p> CPF: {{ cpfAluno }} </p>
+    <p> CPF após o filtro cpf: {{ cpfAluno || acaoCPF }} </p>
+    <p> CPF após o filtro cpf com a inversão: {{ cpfAluno | acaoCPF | inverter}} </p>
+    <p> CPF apenas com o filtro da inversão:  {{ cpfAluno | inverter}} </p> 
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {}
+  filters: {
+    acaoCPF(valor){ 
+      const arr = valor.split('') 
+      arr.splice(3, 0, '.')
+      arr.splice(7, 0, '.') 
+      arr.splice(11, 0, '-')
+      return arr.join('') 
+    }
+  },
+  data() {
+    return {
+      cpfAluno: '60070080090'
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
 }
 </style>
