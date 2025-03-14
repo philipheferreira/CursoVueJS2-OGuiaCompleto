bbeb315ad12a1daeebe0a27d9061cb2ac40b9074
philipheferreira <philipheferreira@gmail.com>
2025-03-14 16:46:50 -0300

    Feat: module 13 cap 5 delete useless components

--- 13 - Melhorando sua App com filtros e mixins/4-filtro-e-vbind/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/4-filtro-e-vbind/src/App.vue
@@ -6,6 +6,15 @@
     <p> CPF após o filtro cpf: {{ cpfAluno || acaoCPF }} </p>
     <p> CPF após o filtro cpf com a inversão: {{ cpfAluno | acaoCPF | inverter}} </p>
     <p> CPF apenas com o filtro da inversão:  {{ cpfAluno | inverter}} </p> 
+    <hr/>
+    <input type="text" :value="cpfAluno"> <!-- v-model não suporta esse tipo de estrutura -->
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF">
+    <br />
+    <input type="text" :value="cpfAluno | inverter ">
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF | inverter">
+
 	</div>
 </template>
 
@@ -13,11 +22,11 @@
 export default {
   filters: {
     acaoCPF(valor){ 
-      const arr = valor.split('') 
-      arr.splice(3, 0, '.')
-      arr.splice(7, 0, '.') 
-      arr.splice(11, 0, '-')
-      return arr.join('') 
+      const arr = valor.split('') // divite a string em array
+      arr.splice(3, 0, '.') // adiciona um ponto na 3° posição sem deletar ninguém
+      arr.splice(7, 0, '.') // adiciona um ponto na 7° posição sem deletar ninguém
+      arr.splice(11, 0, '-') // adiciona um traço na 11° posição sem deletar ninguém
+      return arr.join('') // junta todo mundo em um unico string
     }
   },
   data() {
@@ -37,5 +46,9 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 2.5rem;
+}
+
+input{
+  font-size: 2.5rem;
 }
 </style>
