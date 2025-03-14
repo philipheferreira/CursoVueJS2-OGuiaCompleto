49193620636c877531f02b6cd14825632e2ce58d
philipheferreira <philipheferreira@gmail.com>
2025-03-14 16:47:36 -0300

    Feat: module 13 cap 5 create basic template

--- 13 - Melhorando sua App com filtros e mixins/5-duplicando-codigo-para-usar-os-mixins/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/5-duplicando-codigo-para-usar-os-mixins/src/App.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+    <hr>
+    <p> CPF: {{ cpfAluno }} </p>
+    <p> CPF após o filtro cpf: {{ cpfAluno || acaoCPF }} </p>
+    <p> CPF após o filtro cpf com a inversão: {{ cpfAluno | acaoCPF | inverter}} </p>
+    <p> CPF apenas com o filtro da inversão:  {{ cpfAluno | inverter}} </p> 
+    <hr/>
+    <input type="text" :value="cpfAluno"> <!-- v-model não suporta esse tipo de estrutura -->
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF">
+    <br />
+    <input type="text" :value="cpfAluno | inverter ">
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF | inverter">
+
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {}
+  filters: {
+    acaoCPF(valor){ 
+      const arr = valor.split('') // divite a string em array
+      arr.splice(3, 0, '.') // adiciona um ponto na 3° posição sem deletar ninguém
+      arr.splice(7, 0, '.') // adiciona um ponto na 7° posição sem deletar ninguém
+      arr.splice(11, 0, '-') // adiciona um traço na 11° posição sem deletar ninguém
+      return arr.join('') // junta todo mundo em um unico string
+    }
+  },
+  data() {
+    return {
+      cpfAluno: '60070080090'
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input{
+  font-size: 2.5rem;
 }
 </style>
