be82536cf9fc9cd8579ac9c885afd14e56c114f9
philipheferreira <philipheferreira@gmail.com>
2025-03-15 12:06:15 -0300

    Feat: module 13 cap 6 create basic template

--- 13 - Melhorando sua App com filtros e mixins/6-criando-um-mixin-global-caso-especial/src/App.vue
+++ 13 - Melhorando sua App com filtros e mixins/6-criando-um-mixin-global-caso-especial/src/App.vue
@@ -1,23 +1,75 @@
+<!-- Montando cenario com erro de duplicação -->
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+    <hr>
+    <p> CPF: {{ cpfAluno }} </p>
+    <p> CPF após o filtro cpf: {{ cpfAluno || acaoCPF }} </p>
+    <p> CPF após o filtro cpf com a inversão: {{ cpfAluno | acaoCPF | inverter}} </p>
+    <p> CPF apenas com o filtro da inversão:  {{ cpfAluno | inverter}} </p>
+    <hr/>
+    <input type="text" :value="cpfAluno">
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF">
+    <br />
+    <input type="text" :value="cpfAluno | inverter ">
+    <br />
+    <input type="text" :value="cpfAluno | acaoCPF | inverter">
+    <hr/>
+    <hr/>
+    <Frutas/>
+    <hr/>
+    <div> <!-- lista replicada localmente -->
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta"> {{ fruta }} </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add" /> <!-- Apertando enter adiciona -->
+    </div>
+	</div>
 </template>
 
 <script>
+import Frutas from './components/Frutas.vue'
 
 export default {
-  name: 'App',
-  components: {}
+  components: { Frutas },
+  filters: {
+    acaoCPF(valor){ 
+      const arr = valor.split('')
+      arr.splice(3, 0, '.')
+      arr.splice(7, 0, '.')
+      arr.splice(11, 0, '-')
+      return arr.join('')
+    }
+  },
+  data() {
+    return {
+      cpfAluno: '60070080090',
+      fruta: '',
+      frutas: ['banana', 'maça', 'laranja'] // Lista generica
+    }
+  },
+        methods: {
+            add() {
+                this.frutas.push(this.fruta) // Metodo para adicionar fruta
+                this.fruta = '' // limpa depois de adicionar
+            }
+        }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input{
+  font-size: 2.5rem;
 }
 </style>
---
+++ 13 - Melhorando sua App com filtros e mixins/6-criando-um-mixin-global-caso-especial/src/components/Frutas.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta"> {{ fruta }} </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add" /> <!-- Apertando enter adiciona -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'frutasComponent',
+        data() {
+            return{
+                fruta: '',
+                frutas: ['banana', 'maça', 'laranja'] // Lista generica
+            }
+        },
+        methods: {
+            add() {
+                this.frutas.push(this.fruta) // Metodo para adicionar fruta
+                this.fruta = '' // limpa depois de adicionar
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
