e1c312112ae0f92659cdf4c2916d750b018ddb87
philipheferreira <philipheferreira@gmail.com>
2025-03-18 15:25:54 -0300

    Feat: module 16 cap 7 basic template for start

---
+++ 16 - rotas em uma aplicação VueJS/7-navegacao-via-codigo-navegacao-imperativa/src/components/template/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <nav class="menu">
+        <ul>
+                <router-link to="/" tag="li" active-class="active" exact> <!-- O exact serve para definir que quando exatamente for / ele será ativo, para evitar que ele fique marcado em todas as ocasiões -->
+                    <a>Home</a>
+                </router-link>
+                <router-link to="/usuario" tag="li" active-class="active">
+                    <a>usuarios</a>
+                </router-link>
+        </ul>
+    </nav>
+
+</template>
+
+<script>
+
+    export default{
+        name: "menuComponents"
+    }
+
+</script>
+
+<style>
+
+
+</style>
