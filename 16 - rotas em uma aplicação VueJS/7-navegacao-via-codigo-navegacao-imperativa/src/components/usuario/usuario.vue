28cdaba60e7aa6abec5b15b803dd4ac810e8cd60
philipheferreira <philipheferreira@gmail.com>
2025-03-18 15:54:40 -0300

    Feat: module 16 cap 8 create components

--- 16 - rotas em uma aplicação VueJS/7-navegacao-via-codigo-navegacao-imperativa/src/components/usuario/usuario.vue
+++ 16 - rotas em uma aplicação VueJS/7-navegacao-via-codigo-navegacao-imperativa/src/components/usuario/usuario.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="usuario">
         <h2>Usuários</h2>
+        <hr/>
+        <button sucesso @click="irParaInicio">Voltar</button> <!-- Coloquei um atributo que já está sendo aplicado um style pelo css -->
     </div>
 </template>
 
 <script>
 export default {
-    name: 'usuarioApp'
+    name: 'usuarioApp',
+    methods: {
+        irParaInicio(){
+            // this.$router.push('/') // o push faz com que repasse essa informação para o path declarado
+            this.$router.push({path: '/'})
+        }
+    }
 }
 </script>
 
