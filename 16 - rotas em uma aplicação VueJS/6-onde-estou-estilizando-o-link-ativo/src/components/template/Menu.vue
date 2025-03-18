7c12219163d1cd0bcca776ff8c2d621fa9b4125d
philipheferreira <philipheferreira@gmail.com>
2025-03-18 15:12:45 -0300

    Feat: module 16 cap 5 finish

--- 16 - rotas em uma aplicação VueJS/6-onde-estou-estilizando-o-link-ativo/src/components/template/Menu.vue
+++ 16 - rotas em uma aplicação VueJS/6-onde-estou-estilizando-o-link-ativo/src/components/template/Menu.vue
@@ -1,8 +1,14 @@
 <template>
     
     <nav class="menu">
-        <router-link to="/">Home</router-link>
-        <router-link to="/usuario">usuarios</router-link>
+        <ul>
+                <router-link to="/" tag="li" active-class="active" exact> <!-- O exact serve para definir que quando exatamente for / ele será ativo, para evitar que ele fique marcado em todas as ocasiões -->
+                    <a>Home</a>
+                </router-link> <!-- a tag a ira receber o nome, é necessario apra configurar depois de utilizar a tag para receber a caracteristica de li -->
+                <router-link to="/usuario" tag="li" active-class="active">
+                    <a>usuarios</a>
+                </router-link>
+        </ul>
     </nav>
 
 </template>
