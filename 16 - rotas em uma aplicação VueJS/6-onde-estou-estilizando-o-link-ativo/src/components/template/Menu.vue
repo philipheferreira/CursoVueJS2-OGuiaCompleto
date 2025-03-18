d18304f471b7ada6c976128e3a2400f63fd281cd
philipheferreira <philipheferreira@gmail.com>
2025-03-18 15:00:13 -0300

    Feat: module 16 cap 6 basic template

---
+++ 16 - rotas em uma aplicação VueJS/6-onde-estou-estilizando-o-link-ativo/src/components/template/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+    
+    <nav class="menu">
+        <router-link to="/">Home</router-link>
+        <router-link to="/usuario">usuarios</router-link>
+    </nav>
+
+</template>
+
+<script>
+
+    export default{
+        name: "menuComponents"
+    }
+
+</script>
+
+<style>
+
+
+</style>
