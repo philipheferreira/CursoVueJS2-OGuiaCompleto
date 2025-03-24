f6d24c1e14791006073492af9ed54bf4bc2d93f1
philipheferreira <philipheferreira@gmail.com>
2025-03-24 10:38:04 -0300

    Feat: things i forget

--- 16 - rotas em uma aplicação VueJS/11-parametros-de-rotas-via-props/src/App.vue
+++ 16 - rotas em uma aplicação VueJS/11-parametros-de-rotas-via-props/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -10,9 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
