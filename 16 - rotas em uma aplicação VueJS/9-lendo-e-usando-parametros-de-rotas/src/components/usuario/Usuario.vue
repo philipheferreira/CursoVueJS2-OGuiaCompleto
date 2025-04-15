92d547a43ac2235cd8233681679e71b74cc6ef79
philipheferreira <philipheferreira@gmail.com>
2025-04-15 15:23:30 -0300

    Feat: module 16 cap 9 finish

--- 16 - rotas em uma aplicação VueJS/9-lendo-e-usando-parametros-de-rotas/src/components/usuario/Usuario.vue
+++ 16 - rotas em uma aplicação VueJS/9-lendo-e-usando-parametros-de-rotas/src/components/usuario/Usuario.vue
@@ -2,6 +2,7 @@
     <div class="usuario">
         <h2>usuários</h2>
         <hr/>
+        <p><strong>Código:</strong> {{ id }} </p>
         <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
@@ -9,6 +10,13 @@
 <script>
 export default {
     name:'usuarioApp',
+        data() {
+            return{
+                id: this.$route.params.id /* forma que tem para pegar o parametro a partir da rota. 
+                O route é referente a rota, o params é referente ao parametro da rota na url, o id é a
+                variavel definida */
+            }
+        },
     methods: {
         irParaInicio(){
             this.$router.push({path: '/'})
