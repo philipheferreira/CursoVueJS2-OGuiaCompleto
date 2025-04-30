d07656db50332a21b95084b0a131e7e3a230bea1
philipheferreira <philipheferreira@gmail.com>
2025-04-30 15:45:14 -0300

    Feat: module 17 cap 11 basic template create

--- 17 - melhor gerenciamento de estado com vuex/11-usando-mutations/src/main.js
+++ 17 - melhor gerenciamento de estado com vuex/11-usando-mutations/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store/store'
 
 Vue.config.productionTip = false
 
+import Painel from './components/Painel'
+Vue.component('componentPainel', Painel)
+
+Vue.filter('dinheiro', valor => {
+	return `R$ ${parseFloat(valor).toFixed(2)}`.replace('.', ',')
+})
+
 new Vue({
-  render: h => h(App),
+	store,
+	render: h => h(App),
 }).$mount('#app')
