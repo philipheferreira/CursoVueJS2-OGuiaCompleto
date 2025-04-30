3453139621444c608962fc265e6dfe6197120212
philipheferreira <philipheferreira@gmail.com>
2025-04-30 16:34:57 -0300

    Feat: module 17 cap 11 create mutations

--- 17 - melhor gerenciamento de estado com vuex/11-usando-mutations/src/store/store.js
+++ 17 - melhor gerenciamento de estado com vuex/11-usando-mutations/src/store/store.js
@@ -4,17 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
+    state: { // representa o estado compartilhado da aplicação
         produtos: [
             { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
             { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
             { nome: 'Produto 3', quantidade: 1, preco: 43.18 }
         ]
     },
-    getters: {
-        valorTotal(state){
+    getters: { // Funções que vão ler o estado e fazer uma especie de processamento. Recebem o valor estado e fazer um trabalho e modificação com o valor do dado
+        valorTotal(state) {
             return state.produtos.map(p => p.quantidade * p.preco)
             .reduce((total, atual) => total + atual, 0)
+        }
+    },
+    mutations: { // varias funções que tem a responsabilidade de alterar o estado global da aplicação controlado pelo vuex => são como os setters
+        adicionarProduto(state, payload) {
+            state.produtos.push(payload)
         }
     }
 })
