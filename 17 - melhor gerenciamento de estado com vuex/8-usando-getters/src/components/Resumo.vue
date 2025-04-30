311ad406860df853a717a99b8de385c3774a735c
philipheferreira <philipheferreira@gmail.com>
2025-04-30 14:03:28 -0300

    Feat: module 17 cap 8 finish

---
+++ 17 - melhor gerenciamento de estado com vuex/8-usando-getters/src/components/Resumo.vue
@@ -0,0 +1,34 @@
+<template>
+    <componentPainel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </componentPainel>
+</template>
+
+<script>
+export default {
+    name: 'componentResumo',
+    computed: {
+        produtos() { // faz o acesso da aplicação vuex apontando a aplicação store criada globalmente pelo main.js. acessa a classe produtos criada
+            return this.$store.state.produtos
+        },
+        total() {
+            return this.$store.getters.valorTotal // puxa o valor declarado dentro de getters
+        }
+    }
+}
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        border-top: 1px solid #EEE;
+        width: 33%;
+    }
+</style>
