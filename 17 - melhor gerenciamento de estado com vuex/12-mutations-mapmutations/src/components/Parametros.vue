4d9ccabf0857809343239b0a70d96d21a48deacd
philipheferreira <philipheferreira@gmail.com>
2025-05-01 18:13:02 -0300

    Feat: module 17 cap 12 create basic template

---
+++ 17 - melhor gerenciamento de estado com vuex/12-mutations-mapmutations/src/components/Parametros.vue
@@ -0,0 +1,33 @@
+<template>
+    <componentPainel titulo="Parâmetros" vermelho>
+        <div class="parametros">
+            <span>
+                <strong>Quantidade Padrão: </strong> 
+                <input type="number" v-model="quantidade">
+            </span>
+            <span>
+                <strong>Preço Padrão: </strong>
+                <input type="number" v-model="preco">
+            </span>
+        </div>
+    </componentPainel>
+</template>
+
+<script>
+export default {
+    name: 'componentParametros',
+    data() {
+        return {
+            quantidade: 0,
+            preco: 0
+        }
+    }
+}
+</script>
+
+<style>
+    .parametros {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
