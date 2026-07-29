7c24b77f01f0c546c71661b9d4ff2731ba34a243
philipheferreira <philipheferreira@gmail.com>
2026-07-29 12:53:59 -0300

    create basic projects for cap 13 routers

---
+++ 16 - rotas em uma aplicação VueJS/13-navegando-para-rotas-aninhadas/src/components/usuario/Usuario.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="usuario">
+        <h2>usuários</h2>
+        <hr/>
+        <router-view />
+        <button sucesso @click="irParaInicio">Voltar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'usuarioApp',
+    props: ['id'],
+    methods: {
+        irParaInicio(){
+            // this.$router.push('/') // o push faz com que repasse essa informação para o path declarado
+            this.$router.push({path: '/'})
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 16 - rotas em uma aplicação VueJS/13-navegando-para-rotas-aninhadas/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuario Editar</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
