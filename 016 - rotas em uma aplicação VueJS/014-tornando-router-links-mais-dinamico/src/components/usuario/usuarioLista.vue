11ae1e5bae8b9f7412a8086f42e1b913e7620366
philipheferreira <philipheferreira@gmail.com>
2026-07-29 18:23:56 -0300

    create basic project in new cap 14

---
+++ 016 - rotas em uma aplicação VueJS/014-tornando-router-links-mais-dinamico/src/components/usuario/usuarioLista.vue
@@ -0,0 +1,22 @@
+/*
+
+Cada uma dessas opcoes agora navega para a pagina detalhe
+
+*/
+
+<template>
+    <div class="usuario-lista">
+        <h3>Lista de Usuarios</h3>
+        <router-link tag="li" to="/usuario/1" class="item-lista">Usuario 1</router-link>
+        <router-link tag="li" to="/usuario/2" class="item-lista">Usuario 2</router-link>
+        <router-link tag="li" to="/usuario/3" class="item-lista">Usuario 3</router-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
